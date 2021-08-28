72f2736d995c4b70756a99dba581e83ca3e437a7
playdog-io <cqdxcs3@163.com>
2021-08-28 07:09:00 +0000

    定义kitty,kitty id,和3个storage

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -9,6 +9,13 @@ pub mod pallet {
     use codec::{Encode, Decode};
     use sp_io::hashing::blake2_128;
 
+    #[derive(Encode, Decode)]
+    pub struct Kitty(pub [u8; 16]);
+    //a1,0555,定义一个16字节的u8类型来存储小猫,这样可以通过256位哈希函数获取.
+
+    type KittyIndex = u32;
+    //a2,定义小猫id.
+
     #[pallet::config]
     pub trait Config: frame_system::Config {
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
@@ -22,6 +29,21 @@ pub mod pallet {
     #[pallet::metadata(T::AccountId = "AccountId")]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {}
+
+    #[pallet::storage]
+    #[pallet::getter(fn kitties_count)]
+    pub type KittiesCount<T> = StorageValue<_, u32>;
+    //a3,要记录每个kitty的index,就要记录kitty的总数.通过此方法可以获得当前的总数值.
+
+    #[pallet::storage]
+    #[pallet::getter(fn kitties)]
+    pub type Kitties<T> = StorageMap<_, Blake2_128Concat, KittyIndex, Option<Kitty>, ValueQuery>;
+    //a4,存储kitties数据,以index为key.数据为value存储到map中.Blake2_128Concat是哈希函数的名字,映射时使用它为方法.
+
+    #[pallet::storage]
+    #[pallet::getter(fn owner)]
+    pub type Owner<T: Config> = StorageMap<_, Blake2_128Concat, KittyIndex, Option<T::AccountId>, ValueQuery>;
+    //a5,0853e 存储owner,与上条相同,用id作为key.但存储值为AccountId
 
     #[pallet::error]
     pub enum Error<T> {}
